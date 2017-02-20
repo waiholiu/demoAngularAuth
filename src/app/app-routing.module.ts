52b01c518ee8a96978f01d84d3608a4f979b6bc1
waiholiu <waiholiu>
2017-02-20 23:09:59 +1100

    got it binding to radio box

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: '', component: HomeComponent 
   }
 ];
 
@@ -14,3 +14,6 @@ const routes: Routes = [
   providers: []
 })
 export class AppRoutingModule { }
+
+
+
